aaf974e34920cac28b0ab3b1034e3c7f97c98123
liakosV <billiak@hotmail.com>
2026-06-20 15:06:29 +0300

    feat: add lombok annotations for Message class

--- src/main/java/com/project/messenger/model/Message.java
+++ src/main/java/com/project/messenger/model/Message.java
@@ -1,7 +1,15 @@
 package com.project.messenger.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message extends AbstractEntity {
 
     @Id
