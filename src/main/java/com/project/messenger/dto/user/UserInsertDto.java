71f58c0d5a9c861036d3eb7c5b3f6b28fd51cb4e
liakosV <billiak@hotmail.com>
2026-06-20 15:07:30 +0300

    feat: add user insert DTO

---
+++ src/main/java/com/project/messenger/dto/user/UserInsertDto.java
@@ -0,0 +1,39 @@
+package com.project.messenger.dto.user;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInsertDto {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9._-]+$",
+            message = "Username can only contain letters, numbers, dots, underscores and hyphens")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[@#$%!^&*]).{8,}$", message = "Invalid Password")
+    private String password;
+
+    @NotNull(message = "Date of birth is required")
+    @Past(message = "Date of birth must be in the past")
+    private LocalDate dateOfBirth;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+            regexp = "^\\+?[0-9]{10,15}",
+            message = "Phone number must contain 10 to 15 digits and may start with +"
+    )
+    private String phoneNumber;
+}
