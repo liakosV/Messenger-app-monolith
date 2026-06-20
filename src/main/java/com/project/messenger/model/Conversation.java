4030adf474c859fc97c6f1598b29f17c45e3d468
liakosV <billiak@hotmail.com>
2026-06-20 14:43:23 +0300

    feat: add annotation in Conversation class

--- src/main/java/com/project/messenger/model/Conversation.java
+++ src/main/java/com/project/messenger/model/Conversation.java
@@ -1,12 +1,20 @@
 package com.project.messenger.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Conversation extends AbstractEntity {
 
     @Id
