a210b896e7917e84b5036e4501347f2014d041ec
liakosV <billiak@hotmail.com>
2026-06-20 15:06:54 +0300

    feat: add message insert DTO

---
+++ src/main/java/com/project/messenger/dto/message/MessageInsertDto.java
@@ -0,0 +1,17 @@
+package com.project.messenger.dto.message;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageInsertDto {
+
+    @NotBlank(message = "Message content is required")
+    @Size(max = 2000, message = "Message cannot be more than 2000 characters")
+    private String content;
+}
