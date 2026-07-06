05c6123db0114e76a3c465feda9b6bcb2bc10ea4
liakosV <billiak@hotmail.com>
2026-07-06 20:28:57 +0300

    feat: Add Auth response dto for authentication token

---
+++ src/main/java/com/project/messenger/dto/authentication/AuthResponseDto.java
@@ -0,0 +1,12 @@
+package com.project.messenger.dto.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDto {
+    private String token;
+}
