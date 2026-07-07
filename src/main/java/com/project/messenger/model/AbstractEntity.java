1b0ac34ab0b3effa9b8627a953fbb797881f54ca
liakosV <billiak@hotmail.com>
2026-07-07 20:01:44 +0300

    persistence: store entity UUIDs as CHAR
    Configure Hibernate to map UUID fields to CHAR instead of the default binary type so MySQL schema generation does not create UUID columns as BLOB.

--- src/main/java/com/project/messenger/model/AbstractEntity.java
+++ src/main/java/com/project/messenger/model/AbstractEntity.java
@@ -9,6 +9,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -24,6 +26,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public abstract class AbstractEntity {
 
+    @JdbcTypeCode(SqlTypes.CHAR)
     @Column(unique = true, nullable = false, updatable = false)
     private UUID uuid;
 
